732b341f51b19d6864710b047725686efa2cea7b
Rivaldi <cbot59@gmail.com>
2020-08-21 21:18:27 +0700

    Menerapkan Webpack pada Club Finder

---
+++ ClubFinder/src/app.js
@@ -0,0 +1,6 @@
+import './styles/style.css'
+import main from './script/view/main.js';
+import './script/component/app-bar.js';
+import './script/component/search-bar.js';
+
+document.addEventListener("DOMContentLoaded", main);
